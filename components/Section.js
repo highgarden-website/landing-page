b6c67fc890f4ec1c0c41c27fdc0364a39dcbd04f
Franco Arza <franco@basement.studio>
2019-09-28 20:04:21 -0300

    add Section component

--- components/Section.js
+++ components/Section.js
@@ -1,13 +1,30 @@
-export default function Section({ children }) {
+export default function Section({ children, columns = 1, noMargin }) {
+  const columnsInt = parseInt(columns, 10)
+
   return (
-    <section>
+    <section className="section">
       {children}
       <style jsx>{`
-        section {
-          background-color: var(--gray-4);
-          height: 100vh;
-          width: 100vw;
-          max-width: 100%;
+        .section {
+          display: grid;
+          grid-template-columns: repeat(
+            ${columnsInt},
+            minmax(calc(1000px / ${columnsInt + 20}), 1fr)
+          );
+          grid-column-gap: 20px;
+          grid-row-gap: 40px;
+          margin: ${noMargin ? "0" : "40px 0"};
+          margin-bottom: ${noMargin ? "0" : "80px"};
+        }
+
+        .section + .section {
+          margin-top: 0;
+        }
+
+        @media (max-width: 1050px) {
+          .section {
+            grid-template-columns: 1fr;
+          }
         }
       `}</style>
     </section>
