3b97b88a91114688529b58b7add9eefb0bf93d69
Leandro Soengas <leandro@basement.studio>
2020-09-16 13:54:48 -0300

    remove option Fixed Income

--- components/Contact.js
+++ components/Contact.js
@@ -119,7 +119,7 @@ function Form() {
           name="strategy"
           placeholder={
             <div style={{ color: "rgba(255, 255, 255, 0.7)" }}>
-              Elegi entre los 3 tipos de inversion
+              Elegi entre los 2 tipos de inversion
             </div>
           }
           isSearchable={false}
